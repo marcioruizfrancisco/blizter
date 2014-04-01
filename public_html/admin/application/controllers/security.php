71e217ff127d86bd52f89c46f8fe0f038b789a0c
Brajola <brajola@gmail.com>
2014-04-01 15:27:21 -0300

    Remoção do Menu da Área de Login

--- public_html/admin/application/controllers/security.php
+++ public_html/admin/application/controllers/security.php
@@ -8,16 +8,7 @@ class Security extends CI_Controller {
 	
 	private final function go($method, $data=array(), $ajax=false){
 		$data['title'] = $this->parametro_model->get('system_name');
-		
-		if(!$ajax){
-			$this->load->view('common/header', $data);
-		}
-		
 		$this->load->view($this->router->class.'/'.$method, $data);
-		
-		if(!$ajax){
-			$this->load->view('common/footer', $data);
-		}
 	}
 	
 	public function login(){
