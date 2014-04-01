90cb765b15e7e06990b3e3fec39a06b420103e81
Brajola <brajola@gmail.com>
2014-04-01 15:25:21 -0300

    Estrutura Básica do Menu

--- public_html/admin/application/views/common/header.php
+++ public_html/admin/application/views/common/header.php
@@ -2,7 +2,13 @@
 <html>
 	<head>
 		<meta charset="ISO-8859-1">
+		<link rel="stylesheet" type="text/css" href="<?=$this->parametro_model->get('utils_address')?>css/reset.css" />
+		<link rel="stylesheet" type="text/css" href="<?=$this->parametro_model->get('utils_address')?>css/admin-layout.css" />
+		<link rel="stylesheet" type="text/css" href="<?=$this->parametro_model->get('utils_address')?>css/admin-menu.css" />
 		<title><?=$title?></title>
 	</head>
 	<body>
+		<?php $this->load->view('common/menu');?>
 		
+		<div id="container">
+			
---
+++ public_html/admin/application/views/common/menu.php
@@ -0,0 +1,43 @@
+<ul id="menu">
+	<img src="<?=$this->parametro_model->get('images_address')?>system/admin-logo.png" border="0" />
+	
+	<li class="first">
+		<a href="#" class="drop">Configurações</a>
+		<div class="dropdown_4columns">
+			<div class="col_4">
+				<h2>Configurações do Sistema</h2>
+			</div>
+			
+			<div class="col_1">
+				<h3>Segurança</h3>
+				<ul>
+					<li><a href="#">Usuários do Sistema</a></li>
+					<li><a href="#">Grupos de Usuários</a></li>
+					<li><a href="#">Módulos Instalados</a></li>
+					<li><a href="#">Permissões de Acesso</a></li>
+					<li><a href="#">Backup de Permissões</a></li>
+				</ul>
+			</div>
+			
+			<div class="col_1">
+				<h3>Configurações</h3>
+				<ul>
+					<li><a href="#">Parametrização</a></li>
+					<li><a href="#">Processos do Sistema</a></li>
+					<li><a href="#">Replicação de Dados</a></li>
+					<li><a href="#">Backup e Manutenção</a></li>
+				</ul>
+			</div>
+			
+			<div class="col_1">
+				<h3>Ferramentas</h3>
+				<ul>
+					<li><a href="#">Console SQL</a></li>
+					<li><a href="#">Editor de Textos</a></li>
+					<li><a href="#">Instalação de Módulos</a></li>
+					<li><a href="#">Backup do Sistema</a></li>
+				</ul>
+			</div>
+		</div>
+	</li>
+</ul>
